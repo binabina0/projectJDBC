cf6fede8e775124bd4a7af4615a128646b914106
binabina0 <157973071+binabina0@users.noreply.github.com>
2024-05-17 02:07:50 +0600

    created branch without-tutor rebased branch1 onto without-tutor

--- src/main/java/Zavkhoz/DBUtils.java
+++ src/main/java/Zavkhoz/DBUtils.java
@@ -1,4 +1,24 @@
 package Zavkhoz;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
 public class DBUtils {
+    private static String dbURL = "jdbc:h2:mem:test;INIT=RUNSCRIPT FROM 'classpath:init.sql'";
+    private static String dbUsername = "sa";
+    private static String dbPassword = "";
+
+    public static Connection getConnection() {
+
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
 }
---
+++ src/main/java/user/user.java
@@ -0,0 +1,38 @@
+package user;
+
+public class user {
+    protected String userName;
+    public String password;
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "userName='" + userName + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+
+    public user() {
+    }
+
+    public user(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
